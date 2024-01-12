e921624bb6b88428cfb9555430f8ccb7c474266a
Eyowas Habtamu <142387406+EyowasH@users.noreply.github.com>
2024-01-12 09:25:23 +0000

    update

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdOutlineDateRange } from "react-icons/md";
 // import { Link } from "react-router-dom";
 
 const Blogs = () => {
@@ -31,10 +32,12 @@ const Blogs = () => {
 
   return (
     <div className="w-full bg-[#f9f9f9] py-[50px]">
+      
+      <div className="max-w-[1240px] mx-auto">
       <h1 className="font-bold text-3xl my-1 pb-4 text-center">
+      
         Read Our Latest News
       </h1>
-      <div className="max-w-[1240px] mx-auto">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black">
           {blogs.map(
             (blog) => (
@@ -42,7 +45,10 @@ const Blogs = () => {
               <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
                 <img className="h-56 w-full object-cover" src={blog.coverImg} />
                 <div className="p-8">
-                  <p className="">12/1/2024</p>
+                  <div className="flex flex-row">
+                  <MdOutlineDateRange />&nbsp;&nbsp;
+                  <p >12/1/2024</p>
+                  </div>
                   <h3 className="font-bold text-2xl my-1">{blog.title}</h3>
                   <p className="text-gray-600 text-xl">{blog.desc}</p>
                 </div>
