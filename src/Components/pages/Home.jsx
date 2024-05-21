53ba3ed26f8356ecdbb4316730065905006a7cd4
Eyowas Habtamu <142387406+EyowasH@users.noreply.github.com>
2024-05-21 21:01:14 +0300

    Update Home.jsx

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,54 +1,54 @@
-import React from "react";
-import Navbar from "../Navbar";
-import Header from "../Header";
-import BannerDetails from "../BannerDetails";
-import CallToAction from "../CallToAction";
-import Footer from "../Footer";
-import BlogsE from "../BlogsE";
-import Soybean from "../../assets/Soybean.jpg";
-import Fire from "../../assets/Fire.jpg";
-import FlashCard from "../flashDeals/FlashCard";
-import Blogs from "../News";
-import AboutH from "../AboutH";
+import React from 'react'
+import Navbar from '../Navbar'
+import Header from '../Header'
+import BannerDetails from '../BannerDetails'
+import CallToAction from '../CallToAction'
+import Footer from '../Footer'
+import BlogsE from '../BlogsE'
+import Soybean from '../../assets/Soybean.jpg'
+import Fire from '../../assets/Fire.jpg'
+import FlashCard from '../flashDeals/FlashCard'
+import Blogs from '../News'
+import AboutH from '../AboutH'
 
 const images = [
-  "https://via.placeholder.com/300x300/f00/fff.png?text=Product+1",
-  "https://via.placeholder.com/300x300/0f0/fff.png?text=Product+2",
-  "https://via.placeholder.com/300x300/00f/fff.png?text=Product+3",
-  "https://via.placeholder.com/300x300/ff0/fff.png?text=Product+4",
-  "https://via.placeholder.com/300x300/f0f/fff.png?text=Product+5",
-  "https://via.placeholder.com/300x300/0ff/fff.png?text=Product+6",
-];
+    'https://via.placeholder.com/300x300/f00/fff.png?text=Product+1',
+    'https://via.placeholder.com/300x300/0f0/fff.png?text=Product+2',
+    'https://via.placeholder.com/300x300/00f/fff.png?text=Product+3',
+    'https://via.placeholder.com/300x300/ff0/fff.png?text=Product+4',
+    'https://via.placeholder.com/300x300/f0f/fff.png?text=Product+5',
+    'https://via.placeholder.com/300x300/0ff/fff.png?text=Product+6',
+]
 
-const Home = ({ blogs }) => {
-  return (
-    <>
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <AboutH />
-      <Blogs blogs={blogs} />
-      {/* <Brand/> */}
-      {/* <Products/> */}
-      {/* <Update/> */}
-      <div className="p-4 bg-white">
-        <FlashCard images={images} />
-      </div>
-      <BannerDetails
-        Text={"Our Export Products"}
-        img={Soybean}
-        Text1={"Stay in touch: Farmers, Ethiopia."}
-        Text2={
-          "Provide international markets: Local producers, reliable support."
-        }
-        Text3={"Access to products: Oilseeds, pulses."}
-        Textp={
-          "We provide international markets to local producers and reliable support to importers abroad."
-        }
-      />
-      <BlogsE />
-      {/* <BannerDetails
+const Home = () => {
+    return (
+        <>
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <AboutH />
+            {/* <Blogs blogs={blogs} /> */}
+            {/* <Brand/> */}
+            {/* <Products/> */}
+            {/* <Update/> */}
+            <div className="p-4 bg-white">
+                <FlashCard images={images} />
+            </div>
+            <BannerDetails
+                Text={'Our Export Products'}
+                img={Soybean}
+                Text1={'Stay in touch: Farmers, Ethiopia.'}
+                Text2={
+                    'Provide international markets: Local producers, reliable support.'
+                }
+                Text3={'Access to products: Oilseeds, pulses.'}
+                Textp={
+                    'We provide international markets to local producers and reliable support to importers abroad.'
+                }
+            />
+            <BlogsE />
+            {/* <BannerDetails
         reverse={true}
         Text={"Our Import Products"}
         img={Fire}
@@ -59,13 +59,13 @@ const Home = ({ blogs }) => {
           "We hope for the best and prepare for the worst. Fire safety equipment is hopefully something you’ll never have to use."
         }
       /> */}
-      {/* <Features/> */}
-      <CallToAction />
-      <div className="gradient__bg">
-        <Footer />
-      </div>
-    </>
-  );
-};
+            {/* <Features/> */}
+            <CallToAction />
+            <div className="gradient__bg">
+                <Footer />
+            </div>
+        </>
+    )
+}
 
-export default Home;
+export default Home
